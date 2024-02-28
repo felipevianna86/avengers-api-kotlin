eda3bf445dd23b91f2328a1aa7ac9882abfdb318
Felipe Vianna <felipe.vianna86@gmail.com>
2024-02-27 23:16:12 -0300

    Commit initial

---
+++ src/main/kotlin/com/example/avengers/avengersapikotlin/AvengersApiKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.avengers.avengersapikotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvengersApiKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<AvengersApiKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/avengers/avengersapikotlin/AvengersApiKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.avengers.avengersapikotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApiKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
