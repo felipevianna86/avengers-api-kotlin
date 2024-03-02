38559a55174ae5022e5c0e31865feb79edd77a89
Felipe Vianna <felipe.vianna86@gmail.com>
2024-03-01 21:25:30 -0300

    Concluindo resource de Avenger adicionando método delete.

--- src/main/kotlin/com/example/avengers/avengersapikotlin/aplicacao/controller/AvengerResource.kt
+++ src/main/kotlin/com/example/avengers/avengersapikotlin/aplicacao/controller/AvengerResource.kt
@@ -5,15 +5,8 @@ import com.example.avengers.avengersapikotlin.domain.dto.AvengerResponse
 import com.example.avengers.avengersapikotlin.domain.ports.repositories.AvengerRepository
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.net.URI
 
 
@@ -51,4 +44,11 @@ class AvengerResource(
                     ResponseEntity.ok(AvengerResponse.from(avenger))
                 }
             }
+
+    @DeleteMapping("/{id}")
+    fun deleteAvenger(@PathVariable("id") id: Long) =
+            repository.delete(id).let {
+                ResponseEntity.ok()
+            }
+
 }
